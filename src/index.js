028e283eb4603ca32f55ad872d4b7f6eb8b9443f
Daniel Butler <76425899+DanielButlerIsMyName@users.noreply.github.com>
2024-09-19 11:01:51 +0200

    new template

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
-/**
-=========================================================
-* Material Kit 2 React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-kit-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import React from "react";
-import * as ReactDOMClient from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "App";
+import { createRoot } from 'react-dom/client';
+import App from "./App";
+import Modal from "react-modal";
 
-const container = document.getElementById("root");
+Modal.setAppElement("#root");
 
-// Create a root.
-const root = ReactDOMClient.createRoot(container);
-
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
